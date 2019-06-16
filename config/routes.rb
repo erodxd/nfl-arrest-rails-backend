2e621a63e4e0196531d7fef0ae6cc2f7055c4bd4
erodxd <emrod718@gmail.com>
2019-06-16 18:42:37 -0400

    added comment & player_img models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :players, only: [:index, :show]
+  resources :comments, only: [:index,:show,:create,:update,:destroy]
+  resources :users, only: [:index,:show,:create,:update,:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
